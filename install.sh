3db039089abb8d19232af923ca0d3d1f4bcb2f55
ピアノねこ PianoCat <61345869+NSK-1010@users.noreply.github.com>
2020-06-06 18:52:58 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
-wget -O inter.zip https://fonts.google.com/download?family=Inter
-mkdir inter
-unzip inter.zip -d inter
-sudo mkdir /usr/share/fonts/Inter
-sudo mv ./inter/static/*.ttf /usr/share/fonts/Inter/
-rm -r inter
-rm inter.zip
+wget -O [zipファイルの名前] [ダウンロードファイルのURL(URLが変わらないもの(Google Fontsなど)がベスト!)]
+mkdir [フォント名]
+unzip [zipファイルの名前] -d [フォント名]
+sudo mkdir /usr/share/fonts/[フォント名]
+sudo mv ./[フォントがあるディレクトリ]/*.ttf /usr/share/fonts/Inter/
+rm -r [フォント名]
+rm [zipファイルの名前]
