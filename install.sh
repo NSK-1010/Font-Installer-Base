07668da590a4dd7b27f62844a44b8a4a8f6dba08
ピアノねこ PianoCat <61345869+NSK-1010@users.noreply.github.com>
2020-07-04 16:01:57 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,38 @@
-#!/bin/bash
+require 'open-uri'
+@path = '[ダウンロードファイルのURL(URLが変わらないもの(Google Fontsなど)がベスト!)]'
 
-wget -O [zipファイルの名前] [ダウンロードファイルのURL(URLが変わらないもの(Google Fontsなど)がベスト!)]
-mkdir [フォント名]
-unzip [zipファイルの名前] -d [フォント名]
-sudo mkdir /usr/share/fonts/[フォント名]
-sudo mv ./[フォントがあるディレクトリ]/*.ttf /usr/share/fonts/[フォント名]/
-rm -r [フォント名]
-rm [zipファイルの名前]
+fileName = "[zipファイルの名前]"
+
+open(fileName, 'rwa+b') do |output|
+  open(@path) do |data|
+    output.write(data.read)
+  end
+end
+
+require "fileutils"
+FileUtils.mkdir("[フォント名]")
+
+require 'zip'
+
+dest = '[フォント名]'
+
+Zip::File.open('[zipファイルの名前]') do |zip|
+  zip.each do |entry|
+    p entry.name
+    zip.extract(entry, dest + entry.name) { true }
+  end
+end
+
+require "fileutils"
+FileUtils.mkdir("/usr/share/fonts/[フォント名]")
+
+require "fileutils"
+
+Dir.glob('*.ttf').each do |fontname|
+FileUtils.mv("[フォントがあるディレクトリ]" + fontname, "/usr/share/fonts/[フォント名]/" + fontname)
+
+reqiure 'fileutils'
+FileUtils.rm_rf("[フォント名]")
+
+require 'fileutils'
+FileUtils.rm("[zipファイルの名前]")
